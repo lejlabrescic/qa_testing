4788cee1bd284d6580b9a8a191763a5e02eeb3a4
lejla.brescic <lejla.brescic@stu.ibu.edu.ba>
2025-04-12 14:03:44 +0200

    Fixed issues with CSV.

--- src/test/java/pages/ContactPage.java
+++ src/test/java/pages/ContactPage.java
@@ -1,11 +1,21 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.FileReader;
+import java.io.Reader;
+import java.time.Duration;
+import java.util.ArrayList;
 
 public class ContactPage {
     WebDriver driver;
+
 
     // Contact link on homepage
     By contactLink = By.xpath("//*[@id=\"shopify-section-sections--16750295384234__header\"]/sticky-header/header/nav/ul/li[3]/a");
@@ -17,18 +27,19 @@ public class ContactPage {
     By orderNumberField = By.xpath("//*[@id=\"0-text-1\"]");
     By phoneField = By.xpath("//*[@id=\"0-phone-1\"]");
     By serialNumberField = By.xpath("//*[@id=\"0-text-2\"]");
-    By uploadFileInput = By.xpath("//*[@id=\"globo-formbuilder-81469\"]/div/div/div/form/div[2]/div/div[1]/div/div[7]/div/div/div/div[1]/button");
+    By uploadFileInput = By.xpath("//*[@id=\"temp-for-0-file2-1_\"]");
     By productUrlField = By.xpath("//*[@id=\"0-url-1\"]");
     By submitButton = By.xpath("//*[@id=\"globo-formbuilder-81469\"]/div/div/div/form/div[2]/div/div[4]/button[2]");
     By successTextElement = By.xpath("//*[@id=\"globo-formbuilder-81469\"]/div/div/div/form/div[2]/div/div[3]/div[1]");
+    By errorMessage = By.xpath("//span[@class='err']");
+    By supportedTypes = By.xpath("//div[@class='gfb__dropzone--placeholder--description']");
+
     public ContactPage(WebDriver driver) {
         this.driver = driver;
     }
-
     public void navigateToContactForm() {
         driver.findElement(contactLink).click();
     }
-
     public void fillContactForm(String channel, String name, String email, String orderNumber,
                                 String phone, String serialNumber, String productUrl) {
         new Select(driver.findElement(purchaseChannel)).selectByVisibleText(channel);
@@ -41,14 +52,34 @@ public class ContactPage {
     }
 
     public void uploadDocument(String filePath) {
-        driver.findElement(uploadFileInput).sendKeys(filePath);
+
+        WebElement uploadElement = driver.findElement(uploadFileInput);
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].style.display = 'block';", uploadElement);
+
+        uploadElement.sendKeys(filePath);
     }
 
     public void submitForm() {
         driver.findElement(submitButton).click();
     }
+
     public String verifySuccessText() {
 
         return driver.findElement(successTextElement).getText();
     }
+    public String verifyErrorMessage() {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement errorMessageText = wait.until(ExpectedConditions.visibilityOfElementLocated(errorMessage));
+
+        return errorMessageText.getText();
+
+    }
+    public String getSupportedTypes() {
+        return driver.findElement(supportedTypes).getText();
+    }
+
+
 }
--- src/test/java/tests/ContactFormTest.java
+++ src/test/java/tests/ContactFormTest.java
@@ -1,19 +1,27 @@
 package tests;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.cdimascio.dotenv.Dotenv;
 import pages.ContactPage;
 import pages.LoginPage;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ContactFormTest {
     WebDriver driver;
     Dotenv dotenv = Dotenv.configure().directory("src").load();
     String baseUrl = dotenv.get("BASE_URL");
     String password = dotenv.get("PASSWORD");
+    String jpgPath = dotenv.get("JPG_PATH");
+    String gifPath = dotenv.get("GIF_PATH");
+    String  csvPath = dotenv.get("CSV_PATH");
 
     @BeforeEach
     public void setUp() {
@@ -35,22 +43,45 @@ public class ContactFormTest {
                 "keks@gmail.com",
                 "123456",
                 "61123456",
-                "SN-987654",
-                "https://product-url.com"
+                "SN-12345",
+                "https://skateboard.com"
         );
 
-        contactPage.uploadDocument("../../assets/test.jpg");
+        contactPage.uploadDocument(jpgPath);
+        Thread.sleep(3000);
         contactPage.submitForm();
-        Thread.sleep(5000); //added for 
+        Thread.sleep(5000); //added for security purpose
         String successText = contactPage.verifySuccessText();
         assertEquals( "Thanks for getting in touch!", successText);
 
     }
 
-    @Test
-    public void testContactFormSubmissionFromCSV() {
 
-    }
+
+    @Test
+    public void testGIFSupport() throws InterruptedException {
+            ContactPage contactPage = new ContactPage(driver);
+            contactPage.navigateToContactForm();
+
+            contactPage.fillContactForm(
+                    "Amazon",
+                    "Keks",
+                    "keks@gmail.com",
+                    "123456",
+                    "61123456",
+                    "SN-12345",
+                    "https://skateboard.com"
+            );
+            assertEquals("Supported format: JPG, JPEG, PNG, GIF, SVG.", contactPage.getSupportedTypes());
+            contactPage.uploadDocument(gifPath);
+            Thread.sleep(3000);
+            contactPage.submitForm();
+            Thread.sleep(6000);
+            String errorMessageText = contactPage.verifyErrorMessage();
+            assertFalse(errorMessageText.contains("gif"));
+
+        }
+
 
     @AfterEach
     public void tearDown() {
@@ -59,3 +90,4 @@ public class ContactFormTest {
         }
     }
 }
+
